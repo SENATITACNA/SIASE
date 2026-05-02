2e27489a7aeb91f99011d1fec71304d9485e6ad8
Gian <helpjdo08@gmail.com>
2026-05-02 16:43:20 -0500

    Cambios para usuario.repository
    Cambios para usuario.repository "SELECT"

--- backend/src/repositories/usuario.repository.js
+++ backend/src/repositories/usuario.repository.js
@@ -1,6 +1,6 @@
 const db = require("../config/db");
 const getInstructores = (callback) => {
-    const sql = "SELECT * FROM instructor";
+    const sql = "SELECT nombre, apellido, estado FROM instructor";
 
     db.query(sql, (err, resultados) => {
         if (err) {
@@ -12,7 +12,7 @@ const getInstructores = (callback) => {
 };
 
 const getDispositivosAlumno = (callback) => {
-    const sql = "SELECT * FROM dispositivos_x_alumno"
+    const sql = "SELECT tipo, marca, modelo, numero_serie FROM dispositivos_x_alumno"
 
     db.query(sql, (err, resultados) => {
         if (err) {
