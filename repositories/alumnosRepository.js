e6b98e942e88c88343770a3b6de33495028085be
Antonio Melendez <melendez.christopher2012@gmail.com>
2026-05-07 14:25:13 -0500

    Update alumnosRepository.js

--- repositories/alumnosRepository.js
+++ repositories/alumnosRepository.js
@@ -1,7 +1,13 @@
 const db = require("../config/db");
 
 const obtenerAlumnoPorId = (id, callback) => {
-  const sql = "SELECT * FROM datos_alumnos WHERE id = ?";
+  const sql = "SELECT id,
+      nombres, apellidos, idsenati,
+      semestre, carrera_id, estado,
+      fecha_creacion, fecha_modificacion,
+      usuario_creacion, usuario_modificacion,
+      contra 
+    FROM datos_alumnos WHERE id = ?";
   db.query(sql, [id], (err, resultados) => {
     if (err) {
       callback(err, null);
