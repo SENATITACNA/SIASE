aa3debdc61a39c2f5eedecf2e3d60523939a6b2b
Jose maria xd <josuemariaquispecervantes@gmail.com>
2026-05-07 14:24:18 -0500

    Revert "frontend de login"

--- backend/Conexion/Conexion.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-// Configuración de la base de datos
-const pool = mysql.createPool({
-  host: '80.241.217.53',
-  user: 'desarrollador',
-  password: 'SENATI',
-  database: 'proyecto_SIASE',
-});
-module.exports = pool;
