66bf40a003ecaee81d98c81328c36bac5c627104
Suppo13CJ <suppo13cj@gmail.com>
2026-05-07 22:51:51 -0500

    Backend 0.2.1
    Se quito el comentario

--- backend/src/controllers/vigilante.controller.js
+++ backend/src/controllers/vigilante.controller.js
@@ -2,7 +2,7 @@ const db = require("../config/db");
 
 const obtenerVigilantes = async (req, res) => {
     try {
-        // Consultamos solo los vigilantes activos
+        
         const [rows] = await db.query("SELECT id, nombre, apellido, turno FROM vigilante WHERE estado = 1");
         res.json(rows);
     } catch (error) {
