839209e4ee6eb37c3e597f154958becc2f16c2e4
cristianch_25 <condoricristian866@gmail.com>
2026-05-07 23:08:05 -0500

    backend_03
    Se agrega:
    datos_alumnos.service
    datos_alumnos.repository

---
+++ backend/src/services/datos_alumno.service.js
@@ -0,0 +1,10 @@
+const datosAlumnosRepository = require("../repositories/datos_alumnos.repository");
+
+const obtenerAlumnoPorId = (id, callback) => {
+    datosAlumnosRepository.obtenerAlumnoPorId(id, (err, alumno) => {
+        if (err) callback(err, null);
+        else callback(null, alumno);
+    });
+};
+
+module.exports = { obtenerAlumnoPorId };
