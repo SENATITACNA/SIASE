4b663d788b37511378567f8e528ee02a7741b852
Jose Quinteros <JoseGabrielQB@outlook.com>
2026-05-08 15:44:53 -0500

    Eliminacion de alumno datos

--- backend/src/repositories/alumnos.repository.js
+++ backend/src/repositories/alumnos.repository.js
@@ -35,3 +35,20 @@ exports.obtenerDatosAlumnoPorId = async (id) => {
   const [rows] = await db.promise().query(sql, [id]);
   return rows[0];
 };
+
+const pool = require('../config/db');
+
+const obtenerAlumnoPorId = (id, callback) => {
+  const sql = "SELECT id, nombres, apellidos, idsenati, semestre, carrera_id, estado, FROM datos_alumnos WHERE id = ?";
+  db.query(sql, [id], (err, resultados) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, resultados[0]);
+    }
+  });
+};
+
+module.exports = {
+  obtenerAlumnoPorId
+};
