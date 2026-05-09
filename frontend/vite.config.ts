ee3d4b5f06a6ea4a5fe8ac90e36ef7c1fc6b578c
Jose Quinteros <JoseGabrielQB@outlook.com>
2026-05-09 18:13:14 -0500

    Cambio a ip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 4000,
     host: true,
     proxy: {
-      '/login': 'http://localhost:3000',
-      '/api': 'http://localhost:3000'
+      '/login': 'http://80.241.217.53/:3000',
+      '/api': 'http://80.241.217.53/:3000'
     }
   }
 })
