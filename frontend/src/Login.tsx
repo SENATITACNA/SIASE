d30aae45195f2cf6599cb275b4404fea10e0231d
Jose maria xd <josuemariaquispecervantes@gmail.com>
2026-05-09 16:50:46 -0500

    Update login API endpoint to new server address

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -17,7 +17,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("http://80.241.217.53:4000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
