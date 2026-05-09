b5dfb09426c4be04d1e4f688f2d9e30ceacc95a6
Gian <helpjdo08@gmail.com>
2026-05-09 18:01:29 -0500

    Correccion de vigilante.routes y alumnos.routes

--- backend/src/routes/alumnos.routes.js
+++ backend/src/routes/alumnos.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const alumnosController = require("../controllers/alumnos.controller");
+const vigilanteController = require("../controllers/vigilante.controller");
 
+router.get("/lista",vigilanteController.getAlumnos.bind(vigilanteController),);
 router.get("/:id", alumnosController.obtenerAlumnoFormateadoPorId);
 router.get("/:id/datos", alumnosController.obtenerDatosAlumnoPorId);
 
--- backend/src/routes/vigilante.routes.js
+++ backend/src/routes/vigilante.routes.js
@@ -2,19 +2,7 @@ const express = require("express");
 const router = express.Router();
 const vigilanteController = require("../controllers/vigilante.controller");
 
-router.get(
-  "/alumnos",
-  vigilanteController.getAlumnos.bind(vigilanteController),
-);
-
-router.put(
-  "/registro/:id/estado",
-  vigilanteController.putEstadoRegistro.bind(vigilanteController),
-);
-
-router.get(
-  "/:vigilante_id",
-  vigilanteController.getVigilantePorId.bind(vigilanteController),
-);
+router.put("/registro/:id/estado",vigilanteController.putEstadoRegistro.bind(vigilanteController),);
+router.get("/:vigilante_id",vigilanteController.getVigilantePorId.bind(vigilanteController),);
 
 module.exports = router;
