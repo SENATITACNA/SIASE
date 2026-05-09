4619a034ad1c8ce9bc581bf1b908df39fe6216e0
Jose Quinteros <JoseGabrielQB@outlook.com>
2026-05-09 17:58:54 -0500

    Cambio de vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000,
+    host: true,
+    proxy: {
+      '/login': 'http://localhost:3000',
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
