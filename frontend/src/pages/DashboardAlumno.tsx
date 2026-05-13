db116cecb4ad3ddcba1cf46a3918ebaaef547c4b
Rodrigorr20 <rodrirosas@gmail.com>
2026-05-13 12:19:23 -0500

    Frontend Datos Alumnos

--- frontend/src/pages/DashboardAlumno.tsx
+++ frontend/src/pages/DashboardAlumno.tsx
@@ -1,16 +1,60 @@
-import Navbar from '../components/Navbar';
+  import { useEffect, useState } from "react";
+  import { useNavigate } from "react-router-dom";
 
-const DashboardAlumno = () => (
-  <div className="layout-wrapper">
-    <Navbar />
-    <div style={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "#eef2f3" }}>
-      <h1>Menú del Alumno</h1>
-      <p>Bienvenido al Sistema Académico</p>
-      <button onClick={() => window.location.href = "/"} style={{ marginTop: "20px", padding: "10px 20px" }}>
-        Cerrar Sesión
-      </button>
-    </div>
-  </div>
-);
+  import Navbar from "../components/Navbar";
+  import AlumnoForm from "../components/AlumnoForm";
 
-export default DashboardAlumno;
+  import { obtenerAlumno }
+  from "../services/alumnoService";
+
+  import type { Alumno }
+  from "../types/alumnos";
+
+  import "../styles/estudiante.css";
+
+  function DashboardAlumno() {
+
+    const [alumno, setAlumno] =
+      useState<Alumno | null>(null);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+      const userData =
+        localStorage.getItem("user");
+
+      if (!userData) {
+        navigate("/login");
+        return;
+      }
+
+      const user = JSON.parse(userData);
+
+      obtenerAlumno(user.id)
+        .then((data) => setAlumno(data))
+        .catch((error) =>
+          console.error(error)
+        );
+
+    }, [navigate]);
+
+    if (!alumno) {
+      return <p>Cargando...</p>;
+    }
+
+    return (
+      <div className="layout-wrapper">
+
+        <Navbar />
+
+        <div className="contenedor-estudiante">
+
+          <AlumnoForm alumno={alumno} />
+        </div>
+
+      </div>
+    );
+  }
+
+  export default DashboardAlumno;
