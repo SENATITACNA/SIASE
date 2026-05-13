6166e5abe2f5972fdb2cc906961af8e639a65f3e
ANGtoBIT <chulolarmrs.t@gmail.com>
2026-05-12 21:22:31 -0500

    cambios lista_asistencia-vigilante

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,11 @@
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// backend/server.js
+import express from "express";
+import cors from "cors";
+import ListaVigilante_alumnos from "./src/routes/ListaVigilante_alumnos.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/listaVigilante_alumnos", ListaVigilante_alumnos);
+app.listen(3000, () => {
+  console.log("Servidor backend corriendo en http://localhost:3000");
 });
