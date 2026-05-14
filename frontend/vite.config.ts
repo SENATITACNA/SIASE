406f741322841f7b63f19401f9e7f51f64904c84
Yann Hz <zturkoty@gmail.com>
2026-05-13 23:21:27 -0500

    fix/ redireccion de localhost a ip dedicada

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BACKEND = 'http://localhost:3000';
+const BACKEND = 'http://80.241.217.53:3000';
 
 export default defineConfig({
   plugins: [react()],
