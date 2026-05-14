2dca6681f10fa802f4bcc3ba2cba07a17ea60c04
Yanira021 <yaniralopez0802@gmail.com>
2026-05-14 15:33:18 -0500

    correccion de cambios ahora nuevo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,26 +1,5 @@
 const mysql = require("mysql2");
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-const db = mysql.createConnection({
-  host: "80.241.217.53",
-  user: "desarrollador",
-  password: "SENATI",
-  database: "proyecto_SIASE"
-});
-
-db.connect(err => {
-  if (err) {
-    console.log("Error conexión:", err);
-  } else {
-    console.log("Base de datos conectada");
-  }
-});
-
-module.exports = db;
-=======
-=======
->>>>>>> 8df41127beb4920abbc5ccfbfd3daf91273fdf6a
 const pool = mysql.createPool({
   host: "80.241.217.53",
   user: "desarrollador",
@@ -38,7 +17,3 @@ pool.getConnection((err, connection) => {
 });
 
 module.exports = pool;
-<<<<<<< HEAD
->>>>>>> a7265d73ab0e803fe6fbf0c2757f999b9a19b49b
-=======
->>>>>>> 8df41127beb4920abbc5ccfbfd3daf91273fdf6a
